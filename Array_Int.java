f6b3a9a6324b878d0a1019f49f6a521d82c1da08
maminigauda <156423709+maminigauda@users.noreply.github.com>
2024-01-12 21:04:11 +0530

    Array of Int type program
    Create an Array of int datatype and store 10 elements in Array

---
+++ Array_Int.java
@@ -0,0 +1,27 @@
+class Array_Int
+{
+public static void main(String[]args)
+{
+	int a[]=new int[10];
+	 a[0]=5;
+	 a[1]=15;
+	 a[2]=25;
+	 a[3]=35;
+	 a[4]=16;
+	 a[5]=19;
+	 a[6]=55;
+	 a[7]=2;
+	 a[8]=4;
+	 a[9]=14;
+	System.out.println(a[0]);
+	System.out.println(a[1]);
+	System.out.println(a[2]);
+	System.out.println(a[3]);
+	System.out.println(a[4]);
+	System.out.println(a[5]);
+	System.out.println(a[6]);
+	System.out.println(a[7]);
+	System.out.println(a[8]);
+	System.out.println(a[9]);
+}
+}
